4d9bb4ffc5ced66cd20c680af89735330dc1c82e
julian <julian.ardila@uptc.edu.co>
2022-05-23 11:44:17 -0500

    Create Home and view of latest products

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="h-10 w-full bg-stone-400 flex justify-between">
+        <router-link class="mx-3 m-auto" to="/">Shop</router-link>
+        <div class="space-x-2 mr-3">
+            <router-link to="/Summer">Summer</router-link>
+            <router-link to="/winter">Winter</router-link>
+            <router-link class="p-2 bg-green-600 rounded-md text-white" to="/winter">Login</router-link>
+            <router-link class="p-2 bg-blue-600 rounded-md" to="/cart">
+                <box-icon class="relative" name='cart' color="#FFF">a</box-icon>
+            </router-link>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <section class="h-1/2 w-full  inline-block bg-slate-500">
+      <div class="text-center h-full w-full p-20">
+        <p class="text-white text-3xl">Welcome to ShopDjango</p>
+        <p class="text-white text-lg p-2">The best shop store online</p>
+      </div>
+    </section>
+    <div>
+      <div>
+        <h2 class="text-black text-center p-4 text-2xl">Latest products</h2>
+      </div>
+      <section class="flex space-x-5 m-10">
+        <div v-for="product in lastestProducts" :key="product.id">
+          <div class="shadow-2xl p-2 bg-white">
+            <figure>
+              <img :src="product.get_thumbnail" alt="">
+            </figure>
+            <h3 class="text-center">{{ product.name }}</h3>
+            <p class="text-center"> {{ product.price }}</p>
+            <p class="text-center">View details</p>          
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
+  data(){
+    return {
+      lastestProducts : []
+    }
+  },
   components: {
-    HelloWorld
-  }
+
+  },
+  methods: {
+    getLastestProducts(){
+      axios.get('/api/v1/lastest-products/')
+      .then(resp => {
+        this.lastestProducts = resp.data
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+  },
+  mounted() {
+    this.getLastestProducts()
+  },
+
 }
 </script>
